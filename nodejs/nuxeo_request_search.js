1d72a5a1aca972e8ad03c0d5a962da875e294228
Vincent Dutat <vdutat@nuxeo.com>
2016-02-15 18:23:16 -0800

    execute query using js lib

---
+++ nodejs/nuxeo_request_search.js
@@ -0,0 +1,53 @@
+var usage = 'Usage: ' + process.argv[0] + ' ' + process.argv[1] + ' <query> [<schemas>]'
+if (process.argv.length < 2) {
+    console.log(usage);
+    process.exit(1);
+}
+
+var nuxeo = require('nuxeo');
+var util = require('util');
+var connectInfo = {
+    baseURL:'http://localhost:8080/nuxeo',
+    username: 'Administrator',
+    password: 'Administrator'
+}
+var docSchemas = ['dublincore'];
+if (process.argv.length >= 3) {
+    docSchemas = process.argv[3].split(',');
+    connectInfo.schemas = docSchemas;
+}
+
+var client = new nuxeo.Client(connectInfo);
+client.connect(function(error, client) {
+    if (error) {
+        // cannot connect
+        throw error;
+    }
+    // OK, the returned client is connected
+    console.log('* Client is connected: ' + client.connected);
+});
+var query = process.argv[2];
+console.log('* query: ' + query);
+console.log('* schemas: ' + docSchemas);
+//query = encodeURI(query);
+//console.log('* query: ' + query);
+
+/************REST API ***************/
+/*
+client.request('path/default-domain/@search/?query=' + query).get(function(error, data) {
+    console.log('* 2 executing query using REST API (request) ...');
+    if (error) {
+      throw error;
+    }
+    console.log(util.inspect(data, {depth: 4, colors: true}));
+});
+*/
+/***************************/
+client.request('/').schemas(docSchemas).path('@search').query({'query':query}).execute(function(error, data) {
+    console.log('* executing query using JS lib (request) ...');
+    if (error) {
+      throw error;
+    }
+    console.log(util.inspect(data, {depth: 5, colors: true}));
+});
+
