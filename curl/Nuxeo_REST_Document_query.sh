a34a8e64d82b17adf7aa34f00f69d443c23cfcc7
Vincent Dutat <vdutat@nuxeo.com>
2016-05-20 09:23:30 -0700

    add Nuxeo_REST_Document_query.sh

---
+++ curl/Nuxeo_REST_Document_query.sh
@@ -0,0 +1,58 @@
+USAGE="Usage: $0 <docpath> <NXQL query> [<comma-separated schemas>]"
+
+if [ $# -lt 2 ]
+then
+    echo -e $USAGE
+    echo
+    exit 1
+fi
+
+if [ -z "$CONF_FILE" ]
+then
+    CONF_FILE=.setenv
+fi
+if [ ! -f "$CONF_FILE" ]
+then
+    echo "No configuration defined. Create a file named '.setenv' or set env. variable CONF_FILE"
+    exit 2
+fi
+. $CONF_FILE
+#cat $CONF_FILE
+
+source utils.sh
+
+DOCUMENT_PATH=$1
+PLAIN_QUERY=$2
+
+SCHEMAS_PARAM="*"
+if [ $# -eq 3 ]
+then
+    SCHEMAS_PARAM="$3"
+fi
+SCHEMAS_REQ_PARAM="-H X-NXproperties:$SCHEMAS_PARAM"
+
+echo "Querying documents ..."
+HTTP_METHOD="GET"
+
+#PLAIN_QUERY="SELECT * FROM Document WHERE ecm:path STARTSWITH '$DOCUMENT_PATH' AND ecm:isCheckedInVersion = 0 AND ecm:currentLifeCycleState <> 'deleted'"
+QUERY=$(urlEncode "$PLAIN_QUERY")
+URL="http://$NUXEO_SERVER/nuxeo/api/v1/repo/${NUXEO_REPOSITORY:-default}/path$DOCUMENT_PATH/@search?query=$QUERY&pageSize=0&&maxResults=0"
+
+REQ_BODY=""
+REQ_PARAMS=""
+echo "$HTTP_METHOD $URL $REQ_BODY $SCHEMAS_REQ_PARAM"
+curl -s -X $HTTP_METHOD  $(curlAuthParams) \
+-H 'Content-Type:application/json' \
+${SCHEMAS_REQ_PARAM} \
+-w 'HTTP return code: %{http_code}\n' -o $0.out \
+$URL
+DOC_JSON=`cat $0.out`
+#echo $CURL_CMD
+
+firstchar=${DOC_JSON:0:1}
+if [ "$firstchar" == "{" ]
+then
+    echo $DOC_JSON|jq .
+else
+    echo $DOC_JSON
+fi
