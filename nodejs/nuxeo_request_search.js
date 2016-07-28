ef3e251c6dda2a8c604893535d97d868d50c83f8
Vincent Dutat <vdutat@nuxeo.com>
2016-07-27 17:24:54 -0700

    1.0.1 compat

--- nodejs/nuxeo_request_search.js
+++ nodejs/nuxeo_request_search.js
@@ -3,12 +3,13 @@ if (process.argv.length < 3) {
     console.log(usage);
     process.exit(1);
 }
-// nuxeo-js-client 0.9.0
-var nuxeo = require('nuxeo');
+// nuxeo-js-client 0.24.0
+var Nuxeo = require('nuxeo');
 var util = require('util');
 var connectInfo = {
     baseURL:'http://localhost:8080/nuxeo',
     auth: {
+        method: 'basic',
         username: 'Administrator',
         password: 'Administrator'
     }
@@ -18,19 +19,11 @@ console.log('* query: ' + query);
 var docSchemas = ['dublincore'];
 if (process.argv.length > 3) {
     docSchemas = process.argv[3].split(',');
-    connectInfo.schemas = docSchemas;
+    //connectInfo.schemas = docSchemas;
 }
 console.log('* schemas: ' + docSchemas);
 
-var client = new nuxeo.Client(connectInfo);
-client.connect(function(error, client) {
-    if (error) {
-        // cannot connect
-        throw error;
-    }
-    // OK, the returned client is connected
-    //console.log('* Client is connected: ' + client.connected);
-});
+var client = new Nuxeo(connectInfo);
 if (process.argv.length > 4) {
     enrichers = process.argv[4].split(',');
     client.header('X-NXenrichers.document', enrichers);
@@ -41,22 +34,10 @@ if (process.argv.length > 4) {
 //query = encodeURI(query);
 //console.log('* query: ' + query);
 
-/************REST API ***************/
-/*
-client.request('path/default-domain/@search/?query=' + query).get(function(error, data) {
-    console.log('* 2 executing query using REST API (request) ...');
-    if (error) {
-      throw error;
-    }
-    console.log(util.inspect(data, {depth: 4, colors: true}));
-});
-*/
-/***************************/
-client.request('/').schemas(docSchemas).path('@search').query({'query':query}).execute(function(error, data) {
+client.request('query?query=' + query).schemas(docSchemas).get().then(function(data) {
     console.log('* executing query using JS lib (request) ...');
-    if (error) {
-      throw error;
-    }
     console.log(util.inspect(data, {depth: 6, colors: true}));
+}).catch(function(error) {
+      throw error;
 });
 
