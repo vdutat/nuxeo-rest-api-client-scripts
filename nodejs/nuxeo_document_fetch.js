67870a064cbe5b2c681e0dcf8da85179949dc5cd
Vincent Dutat <vdutat@nuxeo.com>
2017-03-01 15:09:40 -0800

    improvements: cmd-line parameters, log level, ...

--- nodejs/nuxeo_document_fetch.js
+++ nodejs/nuxeo_document_fetch.js
@@ -1,40 +1,82 @@
-var usage = 'Usage: ' + process.argv[0] + ' ' + process.argv[1] + ' <docpath> [<comma-separated enrichers>]'
-if (process.argv.length < 3) {
-    console.log(usage);
+function mylog(message, silent) {
+    if (!silent) {
+        console.log(message);
+    }
+}
+
+var Nuxeo = require('nuxeo');
+var util = require('util');
+var fs = require('fs');
+var GetOpt = require('node-getopt');
+var Path = require('path');
+
+var getopt = new GetOpt([
+    ['c', 'config-file=ARG', 'connection config file name'],
+    ['s', 'schema=ARG+', 'schema(s) (default: dublincore)'],
+    ['e', 'enricher=ARG+', 'enricher(s)'],
+    ['H', 'header=ARG+', 'header'],
+    ['S', 'silent', 'silent mode (raw JSON)'],
+    ['v', 'verbose', 'verbose mode'],
+    ['h', 'help', 'display this help']
+]);
+getopt.setHelp(
+    'Usage: node ' + Path.basename(process.argv[1]) + ' <document_path> [OPTIONS]\n\n[[OPTIONS]]\n'
+);
+//getopt.bindHelp();
+//opt = getopt.parse(process.argv.slice(2));
+opt = getopt.parseSystem();
+if (opt.argv.length < 1) {
+    getopt.showHelp();
     process.exit(1);
 }
+//console.info(opt);
 
-var nuxeo = require('nuxeo');
-var util = require('util');
-var connectInfo = {
-    baseURL:'http://localhost:8080/nuxeo',
-    auth: {
-        username: 'Administrator',
-        password: 'Administrator'
-    }
+var verbose = opt.options.verbose;
+var silent = opt.options.silent;
+var configFileName = 'localhost_config.json';
+if(opt.options['config-file']) {
+    configFileName = opt.options['config-file'];
 }
-var client = new nuxeo.Client(connectInfo).schema('*');
-var enrichers = process.argv.slice(3).join(',');
-if (enrichers.length > 0) {
-    client.header('X-NXenrichers.document', enrichers);
+mylog('* config file name: ' + configFileName, silent);
+try {
+    fs.accessSync(configFileName, fs.F_OK);
+} catch (e) {
+    mylog("configuration file '" + configFileName + "' does not exist", silent);
+    process.exit(2);
 }
-client.header('depth', 'max');
-client.connect(function(error, client) {
-    if (error) {
-        // cannot connect
-        throw error;
-    }
-    // OK, the returned client is connected
-    //console.log('Client is connected: ' + client.connected);
-});
-var documentPath = process.argv[2];
-console.log('document path: ' + documentPath);
-client.document(documentPath).fetch(function(error, doc) {
-    if (error) {
-        console.log(util.inspect(error, {colors: true}));
-        throw error;
-    }
-    console.log(util.inspect(doc, {colors: true, depth: 7}));
-});
+var connectInfo = JSON.parse(fs.readFileSync(configFileName));
 
+var documentPath = opt.argv[0];
+mylog('* document path: ' + documentPath, silent);
+var docSchemas = ['dublincore'];
+if(opt.options['schema']) {
+    docSchemas = opt.options['schema'];
+}
+mylog('* schemas: ' + docSchemas, silent);
+var enrichers = { document:[]};
+if(opt.options['enricher']) {
+    enrichers.document = opt.options['enricher'];
+    mylog('* enrichers: ' + JSON.stringify(enrichers), silent);
+}
+var reqHeaders = [];
+if(opt.options['header']) {
+    reqHeaders = opt.options['header'];
+    mylog('* headers: ' + JSON.stringify(reqHeaders), silent);
+}
+var nuxeo = new Nuxeo(connectInfo).schemas(docSchemas).enrichers(enrichers);
+for (var i in reqHeaders) {
+    var headerArray = reqHeaders[i].split(':');
+    nuxeo.header(headerArray[0], headerArray[1]);
+}
+//nuxeo.header('depth', 'children');
+nuxeo.repository().fetch(documentPath).then(function(doc) {
+    mylog('* fetching ...', silent);
+    if (silent) {
+        mylog(JSON.stringify(doc));
+    } else {
+        mylog(util.inspect(doc, {depth: 6, colors: true}));
+    }
+}).catch(function(err) {
+    mylog('! ' + err + ' ' + documentPath);
+});
 
