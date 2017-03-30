8889baeb0aa2fa01e42f547ab3572038988111d8
Vincent Dutat <vdutat@nuxeo.com>
2017-03-29 19:09:47 -0700

    use JSON connection config file

--- nodejs/nuxeo_automation_user_tasks.js
+++ nodejs/nuxeo_automation_user_tasks.js
@@ -1,30 +1,50 @@
-var nuxeo = require('nuxeo');
-var util = require('util');
-var connectInfo = {
-    baseURL:'http://localhost:8080/nuxeo',
-	auth: {
-    	username: 'Administrator',
-    	password: 'Administrator'
-	}
+var usage = 'Usage: ' + process.argv[0] + ' ' + process.argv[1] + ' <docpath> [<comma-separated schemas>] [<comma-separated enrichers>]'
+if (process.argv.length < 2) {
+    console.log(usage);
+    process.exit(1);
 }
-var client = new nuxeo.Client(connectInfo);
-client.connect(function(error, client) {
-    if (error) {
-        // cannot connect
-        throw error;
-    }
-    // OK, the returned client is connected
-    console.log('* Client is connected: ' + client.connected);
-});
-client.operation('Workflow.UserTaskPageProvider')
+// nuxeo-js-client 0.24.0
+var Nuxeo = require('nuxeo');
+var util = require('util');
+var fs = require('fs');
+
+var configFileName = 'localhost_config.json';
+if(process.env.CONF_FILE) {
+    configFileName = process.env.CONF_FILE;
+}
+try {
+    fs.accessSync(configFileName, fs.F_OK);
+} catch (e) {
+    console.log("configuration file '" + configFileName + "' does not exist");
+    process.exit(2);
+}
+var connectInfo = JSON.parse(fs.readFileSync(configFileName));
+var documentPath = '/';
+if (process.argv.length >= 2) {
+    documentPath = process.argv[2];
+}
+console.log('* document path: ' + documentPath);
+var docSchemas = ['dublincore'];
+if (process.argv.length > 3) {
+    docSchemas = process.argv[3].split(',');
+    //connectInfo.schemas = docSchemas;
+}
+console.log('* schemas: ' + docSchemas);
+var enrichers = { document:[]};
+if (process.argv.length > 4) {
+    enrichers['document'] = process.argv[4].split(',');
+    console.log('* enrichers: ' + enrichers);
+}
+
+var nuxeo = new Nuxeo(connectInfo);
+nuxeo.operation('Workflow.UserTaskPageProvider')
     .params({
         pageSize:0
     })
-    .execute(function(error, data) {
+    .input(documentPath).execute().then(function(doc) {
     console.log('* Retrieving user tasks ...');
-    if (error) {
-      throw error;
-    }
-    console.log(util.inspect(data, {depth: 4, colors: true}));
+    console.log(util.inspect(doc, {depth: 6, colors: true}));
+}).catch(function(err) {
+    console.log('! ' + err);
 });
 
