edc0e2cca724f79402258f60c7884c18dfc45f64
Vincent Dutat <vdutat@nuxeo.com>
2017-03-29 19:10:54 -0700

    use options

--- nodejs/nuxeo_bulk_document_create.js
+++ nodejs/nuxeo_bulk_document_create.js
@@ -1,55 +1,102 @@
-var usage = 'Usage: ' + process.argv[0] + ' ' + process.argv[1] + ' <folder_docpath> <nbr_documents> [<doctype>] [<docname_prefix>]'
-if (process.argv.length < 4) {
-    console.log(usage);
+// nuxeo-js-client 0.24.0
+var Nuxeo = require('nuxeo');
+var util = require('util');
+var fs = require('fs');
+var GetOpt = require('node-getopt');
+var Path = require('path');
+
+var getopt = new GetOpt([
+    ['c', 'config-file=ARG', 'connection config file name'],
+    ['n', 'nbr-docs=ARG', 'number of documents to create (default: 10)'],
+    ['t', 'doc-type=ARG', 'document type (default: File)'],
+    ['p', 'doc-name-pfx=ARG', 'document name prefix (default: \'Doc \')'],
+    ['s', 'schema=ARG+', 'schema(s) (default: dublincore)'],
+    ['e', 'enricher=ARG+', 'enricher(s)'],
+    ['v', 'verbose', 'verbose mode'],
+    ['h', 'help', 'display this help']
+]);
+getopt.setHelp(
+    'Usage: node ' + Path.basename(process.argv[1]) + ' <parent_document_path> [OPTIONS]\n\n[[OPTIONS]]\n'
+);
+//getopt.bindHelp();
+//opt = getopt.parse(process.argv.slice(2));
+opt = getopt.parseSystem();
+if (opt.argv.length == 0) {
+    getopt.showHelp();
     process.exit(1);
 }
+//console.info(opt);
 
-var nuxeo = require('nuxeo');
-var util = require('util');
-var connectInfo = {
-    baseURL:'http://localhost:8080/nuxeo',
-    auth: {
-        username: 'Administrator',
-        password: 'Administrator'
-    }
+var verbose = opt.options.verbose;
+var configFileName = 'localhost_config.json';
+if(opt.options['config-file']) {
+    configFileName = opt.options['config-file'];
 }
-var client = new nuxeo.Client(connectInfo).schema('*');
-client.connect(function(error, client) {
-    if (error) {
-    // cannot connect
-        throw error;
-    }
-    // OK, the returned client is connected
-    console.log('* Client is connected: ' + client.connected);
-});
-var parentDocumentPath = process.argv[2];
-var nbrDocuments = process.argv[3];
-var docType = 'File';
-if (process.argv.length > 4) {
-    docType = process.argv[4];
+console.log('* config file name: ' + configFileName);
+try {
+    fs.accessSync(configFileName, fs.F_OK);
+} catch (e) {
+    console.log("configuration file '" + configFileName + "' does not exist");
+    process.exit(2);
 }
-var baseDocumentName = 'Doc ';
-if (process.argv.length > 5) {
-    baseDocumentName = process.argv[5] + ' ';
-}
+var connectInfo = JSON.parse(fs.readFileSync(configFileName));
+var parentDocumentPath = opt.argv[0];
 console.log('* parent document path: ' + parentDocumentPath);
+var nbrDocuments = 10;
+if(opt.options['nbr-docs']) {
+    nbrDocuments = opt.options['nbr-docs'];
+}
 console.log('* number of documents to create: ' + nbrDocuments);
-
-var parentDoc = client.document(parentDocumentPath);
-for (i = 0; i < nbrDocuments; i++) {
-    var documentName = baseDocumentName + i;
-    var newDoc = new client.document({
-        'entity-type': 'document',
-        type: docType,
-        name: documentName,
-        properties: {'dc:title': documentName, 'dc:description': 'A Simple ' + docType }
-    });
-    parentDoc.create(newDoc, function(error, doc) {
-        if (error) {
-          throw error;
-        }
-        console.log('* Created ' + doc.type + ' - ' + doc.path)
-        console.log(util.inspect(doc, {colors: true}));
-    });
+var docType = 'File';
+if(opt.options['doc-type']) {
+    docType = opt.options['doc-type'];
+}
+console.log('* document type: ' + docType);
+var baseDocumentName = 'Doc ';
+if(opt.options['doc-name-pfx']) {
+    baseDocumentName = opt.options['doc-name-pfx'];
+}
+console.log('* document name prefix: ' + baseDocumentName);
+var docSchemas = ['dublincore'];
+if(opt.options['schema']) {
+    docSchemas = opt.options['schema'];
+}
+console.log('* schemas: ' + docSchemas);
+var enrichers = { document:[]};
+if(opt.options['enricher']) {
+    enrichers = opt.options['enricher'];
+    console.log('* enrichers: ' + enrichers);
 }
 
+var nuxeo = new Nuxeo(connectInfo).schemas(docSchemas).enrichers(enrichers);
+nuxeo.repository().fetch(parentDocumentPath).then(function(folder) {
+    console.log('* fetching parent document ...');
+    if (verbose) {
+        console.log(util.inspect(folder, {depth: 6, colors: true}));
+    }
+    console.log('* parent document fetched: ' + folder.path);
+    for (i = 0; i < nbrDocuments; i++) {
+        var documentName = baseDocumentName + i;
+        var newDoc = {
+            'entity-type': 'document',
+            type: docType,
+            name: documentName,
+            properties: {'dc:title': documentName, 'dc:description': 'A Simple ' + docType }
+        };
+        nuxeo.repository().create(folder.path, newDoc).then(function(doc) {
+            console.log('* Created ' + doc.type + ' - ' + doc.path)
+            if (verbose) {
+                console.log(util.inspect(doc, {colors: true}));
+            }
+        }).catch(function(err) {
+            console.log('! document creation: ' + err);
+        });
+        /*
+        if ((i % 30) == 0) {
+            setTimeout(function(){ console.log('* sleeping');}, 500);
+        }
+        */
+    }
+}).catch(function(err) {
+    console.log('! fetch parent folder:' + err);
+});
