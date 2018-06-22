4f642e66d2abb0272b0907d7e3687b39c2fc38e4
Vincent Dutat <vdutat@nuxeo.com>
2018-06-22 11:18:57 -0700

    error message for unknown studio project

--- nodejs/connect_check_project_expiration_dates.sh
+++ nodejs/connect_check_project_expiration_dates.sh
@@ -1,11 +1,16 @@
 if [ $# -lt 1 ]
 then
-    echo "missing parameter: Studio project name"
+    echo "! Missing parameter: Studio project name"
     exit 1
 fi
 STUDIO_PROJECT_NAME=$1
 
 PROJECT_UUID=`node nuxeo_search.js "SELECT * FROM Document WHERE ecm:primaryType='ConnectProject' AND connectproject:symbolicName = '${STUDIO_PROJECT_NAME}'" -c connect_config.json -s "*" -S | jq -r .entries[].uid`
+if [ -z "$PROJECT_UUID" ]
+then
+    echo "! Unknown Studio project '$STUDIO_PROJECT_NAME'"
+    exit 2
+fi
 echo "* Project UUID: ${PROJECT_UUID}"
 
 node nuxeo_search.js "SELECT * FROM Document WHERE ecm:primaryType = 'ConnectService' AND connectservice:projects = '${PROJECT_UUID}'" -c connect_config.json -s "*" -S | jq -r '.entries[] | "* Service " + .title + " (" + .uid + "): " + .properties["connectservice:startDate"] + " -> " + .properties["connectservice:endDate"]'
